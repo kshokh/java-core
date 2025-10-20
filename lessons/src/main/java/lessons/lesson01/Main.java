85d9f878f607d5319d1877152f2d908b0d41d9c9
kshokh <128833441+kshokh@users.noreply.github.com>
2025-10-20 11:00:18 -0400

    Lesson 1

---
+++ lessons/src/main/java/lessons/lesson01/Main.java
@@ -0,0 +1,15 @@
+package lessons.lesson01;
+
+public class Main {
+
+    int value = 5;
+    String name = "Mike";
+
+    static final int DEFAULT_VALUE = 0;
+    static final String DEFAULT_NAME = "John";
+// test
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Math.abs(1);
+    }
+}
